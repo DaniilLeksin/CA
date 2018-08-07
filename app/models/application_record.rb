8f08ecd7ce959064ae05bbe94d111661b45ba335
Dan <dan@MacBook-Air-Dan.local>
2018-08-07 13:51:16 +0300

    Add some comments

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,6 +1,6 @@
 class ApplicationRecord < ActiveRecord::Base
   # Handle any custom attributes stored against any model. 
-  # Any model inherited from ApplicationRecord and has t.text (or t.string with length) :custom_attributes field
+  # Any model inherited from ApplicationRecord and with t.text (or t.string with length) :custom_attributes field
   # is able to store custom attributes as hash.
   #
   # e.g. Model Partner
@@ -26,7 +26,20 @@ class ApplicationRecord < ActiveRecord::Base
   #
   # Query via pure SQL (find record by custom attribute)
   # >> sql = "SELECT * FROM partners WHERE custom_attributes LIKE '%email: p_email%';"
-  # >> ActiveRecord::Base.connection.execute(sql)
+  # >> ActiveRecord::Base.connection.execute(sql).last
+  # The dificulty I was faced that sql query results with ActiveSupport::HashWithIndifferentAccess
+  # => {"id"=>1, "custom_attributes"=>"--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess\nemail: p_email\n", "created_at"=>...
+  # If the customer is trying to query custom attribute by some other custom attribute
+  # additional manipilations will require
+  # e.g. (in continuing of above)
+  # >> p.custom_attributes[:district]='p_district'
+  # >> p.save
+  # >> sql = "SELECT * FROM partners WHERE custom_attributes LIKE '%email: p_email%';"
+  # >> q = ActiveRecord::Base.connection.execute(sql).last
+  # => {"id"=>1, "custom_attributes"=>"--- !ruby/hash:ActiveSupport::HashWithIndifferentAccess\nemail: p_email\ndistrict: p_district\n"
+  # >> partner = Partner.find(q['id'])
+  # >> partner.custom_attributes[:district]
+  # => "p_district"
 
   store :custom_attributes	
   self.abstract_class = true
